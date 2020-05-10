97eab1ed39c4d9ca6da571976d00603ba10540d3
Andy Burton <aburton@Burty.local>
2020-05-10 23:13:09 +0100

    tidying up

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,7 +1,7 @@
 
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#"
+chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#?"
 length = input("Length of password: ")
 length = int(length)
 amount = input("Number of passwords: ")
@@ -12,5 +12,4 @@ for p in range(amount):
     password = " "
     for c in range(length):
         password += random.choice(chars)
-
     print(password)
