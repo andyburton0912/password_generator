a83f8d2862d4e64d0966add493d204bc8b0a4d08
Andy Burton <aburton@Burty.local>
2020-05-10 23:04:45 +0100

    Code for amount of passwords added

--- Password_Generator.py
+++ Password_Generator.py
@@ -3,11 +3,14 @@ import random
 
 chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#"
 length = input("Length of password: ")
-
 length = int(length)
+amount = input("Number of passwords: ")
+amount = int(amount)
 
-password = " "
-for c in range(length):
-    password += random.choice(chars)
 
-print(password)
+for p in range(amount):
+    password = " "
+    for c in range(length):
+        password += random.choice(chars)
+
+    print(password)
